e8883bb51f3e9d5266bb62eedb9cb0ce56b11ec3
anuar_mce <nmc74@alumnos.unican.es>
2022-05-12 14:27:57 +0200

    Add intervalo_horas() in main

--- src/main.py
+++ src/main.py
@@ -1,20 +1,21 @@
 from uoc import UOC
-from utils import alarmas, visualizaciones, borrar_alarmas, format_url
+from utils import (
+    alarmas, visualizaciones, borrar_alarmas, intervalo_horas, format_url
+)
 import schedule, time, json
 from os import getenv
-import socket
 
 
 def main():
     SEG_ALARMAR = 10
-    INTERVALO_VISUALIZACIONES = '6-18'
+    INTERVALO_VISUALIZACIONES = intervalo_horas(
+        int(getenv("HORAS_VISUALIZACIONES")))
     BORRAR_ALARMAS = 300
 
     with open('../cfg/credentials.json') as json_file:
         CREDENTIALS = json.load(json_file)
-
+    
     URL_UOC = "{}:{}".format(format_url(getenv("URL_UOC")), getenv("PORT_UOC"))
-    print(URL_UOC, flush=True)
     uoc = UOC(URL_UOC, 'admin', 'admin')
     schedule.every(10).seconds.do(alarmas, uoc, CREDENTIALS, SEG_ALARMAR)
     schedule.every(5).seconds.do(
